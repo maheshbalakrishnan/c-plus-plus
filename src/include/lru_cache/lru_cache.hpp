c9bbeeaa2846069e1e472d701f23bdeb784b9c9a
Mahesh Balakrishnan <maheshbalakrishnan90@gmail.com>
2019-07-22 19:19:07 -0700

    changes

--- src/include/lru_cache/lru_cache.hpp
+++ src/include/lru_cache/lru_cache.hpp
@@ -1,11 +1,34 @@
 #include<unordered_map>
+#include<list>
 
 using namespace std;
 
+typedef std::list<string>::iterator UsageListIterator;
+
 template <class T> class LRUCache {
+    typedef std::pair<UsageListIterator, T> CacheValue;
 private:
-    unordered_map<string, T> mCache;
+    unordered_map<string, CacheValue> mCache;
+    list<string> mUsageList;
     double mCapacity;
+    /**
+     * Gets the least recently used item in the cache.
+     * Returns a key in the cache
+     */
+    string getLRU();
+
+    /**
+     * Evicts a key from the cache
+     */
+    void evictKey(string);
+
+    /**
+     * Records the usage of the key, decreasing the 
+     * likelihood of the key being evicted next
+     */
+    UsageListIterator recordKeyUsage(string);
+
+    UsageListIterator recordKeyUsage(string, UsageListIterator);
 
 public:
     /**
@@ -22,12 +45,12 @@ public:
      * Performs a get using the key provided.
      * Returns a null if not found
      */
-    T get(string key);
+    bool get(string key, T& value);
 
     /**
      * Inserts a new key-value pair into the cache
      * Returns nothing
      */
-    void insert(string key, T value);
+    bool put(string key, T value);
 
 };
---
+++ src/lru_cache/lru_cache.cxx
@@ -0,0 +1,118 @@
+#include<iostream>
+#include "common.hpp"
+#include "data.hpp"
+#include "lru_cache/lru_cache.hpp"
+using namespace std;
+
+/*
+ * Constructor which takes in the maximum capacity of the cache
+ */
+template <class T> LRUCache<T>::LRUCache(double capacity) : mCapacity(capacity) {
+    cout << "Constructing LRUCache\n";
+}
+
+/*
+ * destructor
+ */
+template <class T> LRUCache<T>::~LRUCache() {
+    cout << "Destructing LRUCache\n";
+}
+
+/**
+ * Returns the value if found in the cache, returns
+ * nullptr otherwise
+ */
+template <class T> bool LRUCache<T>::get(string key, T& value) {
+    if(mCache.find(key) == mCache.end()) {
+        return false;
+    }
+    auto usageQueueRecord = recordKeyUsage(key);
+    value = (mCache.get(key)).second;
+    CacheValue insert = { usageQueueRecord, value };
+    mCache.insert(key, insert);
+    return true;
+}
+
+/**
+ * Inserts a new key into the cache or updates an existing key 
+ * with a new value
+ */
+template <class T> bool LRUCache<T>::put(string key, T value) {
+    if(mCapacity == mCache->size()) {
+        // capacity reached, evict!
+        if(!evictKey(getLRU())) {
+            return false;
+        }
+    }
+    auto usageQueueRecord = recordKeyUsage(key);
+    CacheValue insert = { usageQueueRecord, value };
+    this->mCache.insert(key, insert);
+    return true;
+}
+
+/**
+ * Returns the key that is least recently used.
+ * If cache is empty, returns empty string
+ */
+template <class T> string LRUCache<T>::getLRU() {
+    if(mUsageList.size() <= 0) {
+        return "";
+    }
+    string key = mUsageList.back();
+    mUsageList.pop_back();
+    return key;
+}
+
+/**
+ * Removes a key from the cache
+ */
+template <class T> void LRUCache<T>::evictKey(string key) {
+    CacheValue value;
+    if(!get(key, value)) {
+        return;
+    }
+    mUsageList.erase(value.first);
+    mCache.erase(key);
+    return;
+}
+
+/**
+ * Records the usage of a new key
+ */
+template <class T> UsageListIterator LRUCache<T>::recordKeyUsage(string key) {
+    mUsageList.push_front(key);
+    return mUsageList.end();
+}
+
+/**
+ * Records the usage of an existing key
+ */
+template <class T> UsageListIterator LRUCache<T>::recordKeyUsage(string key, UsageListIterator it) {
+    mUsageList.erase(it);
+    return recordKeyUsage(key);
+}
+
+
+
+/**
+ * temporary entry point 
+ */
+int main(int argc, char* argv[]) {
+    UNUSED(argc)
+    UNUSED(argv)
+    LRUCache<Data> lc(5);
+    Data d;
+    lc.put("a", d);
+    lc.put("b", d);
+    lc.put("c", d);
+    lc.put("d", d);
+    lc.put("e", d);
+
+    // should evict a
+    lc.put("f", d);
+
+    Data g;
+    lc.get("a", g);
+    
+    return 0;
+}
